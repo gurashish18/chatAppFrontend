51a1c3f4ff7519ff196bcd7e196a17ce7498b7eb
Gurashish Singh Gill <gurashish22@Gurashishs-MacBook-Air.local>
2023-07-30 10:38:27 +0530

    settings ui

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,5 @@
+/** @format */
+
 import { Suspense, lazy } from "react";
 import { Navigate, useRoutes } from "react-router-dom";
 
@@ -9,31 +11,34 @@ import { DEFAULT_PATH } from "../config";
 import LoadingScreen from "../components/LoadingScreen";
 
 const Loadable = (Component) => (props) => {
-  return (
-    <Suspense fallback={<LoadingScreen />}>
-      <Component {...props} />
-    </Suspense>
-  );
+	return (
+		<Suspense fallback={<LoadingScreen />}>
+			<Component {...props} />
+		</Suspense>
+	);
 };
 
 export default function Router() {
-  return useRoutes([
-    {
-      path: "/",
-      element: <DashboardLayout />,
-      children: [
-        { element: <Navigate to={DEFAULT_PATH} replace />, index: true },
-        { path: "app", element: <GeneralApp /> },
-        
-        { path: "404", element: <Page404 /> },
-        { path: "*", element: <Navigate to="/404" replace /> },
-      ],
-    },
-    { path: "*", element: <Navigate to="/404" replace /> },
-  ]);
+	return useRoutes([
+		{
+			path: "/",
+			element: <DashboardLayout />,
+			children: [
+				{ element: <Navigate to={DEFAULT_PATH} replace />, index: true },
+				{ path: "app", element: <GeneralApp /> },
+				{ path: "settings", element: <Settings /> },
+
+				{ path: "404", element: <Page404 /> },
+				{ path: "*", element: <Navigate to="/404" replace /> },
+			],
+		},
+		{ path: "*", element: <Navigate to="/404" replace /> },
+	]);
 }
 
 const GeneralApp = Loadable(
-  lazy(() => import("../pages/dashboard/GeneralApp")),
+	lazy(() => import("../pages/dashboard/GeneralApp"))
 );
+
+const Settings = Loadable(lazy(() => import("../pages/dashboard/Settings")));
 const Page404 = Loadable(lazy(() => import("../pages/Page404")));
